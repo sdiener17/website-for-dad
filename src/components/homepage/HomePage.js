e79618c0b48af2e464f4620559db719251731dd7
Sarah Diener <sdiener@ksu.edu>
2022-06-07 20:45:33 -0500

    First commit

---
+++ src/components/homepage/HomePage.js
@@ -0,0 +1,31 @@
+import React, {useEffect} from "react";
+import styled from "styled-components";
+
+export default function HomePage(){
+    useEffect(() => {
+        window.scrollTo(0, 0)
+      }, [])
+    return (
+        <PageWrapper>
+            Hello world
+        </PageWrapper>
+    )
+}
+
+
+const PageWrapper = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+    width:100%;
+    padding-top:40px;
+    padding-bottom:60px;
+    //margin-left:40px;
+    .marginTop{
+        margin-bottom:40px;
+    }
+    .marginBottom{
+        margin-bottom:var(--marginBottom);
+    }
+`;
