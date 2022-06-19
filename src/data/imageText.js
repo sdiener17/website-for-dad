6a6eb73a29d7c59fc7fd3072c2fb0c955f4978b5
Sarah Diener <sdiener@ksu.edu>
2022-06-19 13:59:58 -0500

    Updated Caleb's text about page

--- src/data/imageText.js
+++ src/data/imageText.js
@@ -9,22 +9,24 @@ export const imageText = {
         thing ever- Sarah`,
     aboutGallery2:`I remember when you brought home my new desk and I was shocked. It was ugly an a whole other level. But then
         we fixed it up and now it's awesome -Joshua`,
-    aboutGallery3:`-Caleb`,
+    aboutGallery3:`Watching you poorly attempt to play games on the wii with us -Caleb`,
     aboutGallery4:`I distincly recall a time we were on paddle boards and I came up behind you and knocked you off -Joshua`,
     aboutGallery5:`I always love hearing the stories from the cousins about the mischief you got into with them around Christmas
         every year, like putting their lifesavers boxes in a wooden box -Sarah`,
-    aboutGallery6:`-Caleb`,
+    aboutGallery6:`Whooping you in darts and ping pong in the basement -Caleb`,
     aboutGallery7:`You used to sometimes come in and dump ice in on us while we were taking showers and you thought that was just
         sooooo funny -Sarah`,
-    aboutGallery8:`-Caleb`,
-    aboutGallery9:`-Caleb`,
+    aboutGallery8:`I always loved playing catch with frisbees or baseballs or playing soccer in the yard with you -Caleb`,
+    aboutGallery9:`Loved exploring the wilderness with you -Caleb`,
     aboutGallery10:`Sheep have enough water? -Joshua & Sarah`,
-    aboutGallery11:`-Caleb`,
+    aboutGallery11:`Driving home from branding calves at grandmas and trying to guess all the alphabetical road names on that 
+        one highway -Caleb`,
     aboutGallery12:`I remember a distant memory of the time you didn't give us the van keys and we ended up walking 6 miles back
         to a hotel -Joshua`,
     aboutGallery13:`I remember you used to sometimes grab some marshmallows and start throwing them at us whenever you used them
         for something like hot chocolate or rice crispy treats -Sarah`,
-    aboutGallery14:`-Caleb`,
+    aboutGallery14:`All the words of encouragement that you gave me throughout my time in middle school and high school competing 
+        in sports were really helpful -Caleb`,
     aboutGallery15:`I remember after having a toothbrush kicked back into my throat, someone read me stories. I don't remember if 
     it was you or not, but seems like something you'd do to help me calm down -Joshua`,
     aboutGallery16:`You were always there for me when I was struggling with my math homework, and you somehow put up with me even
@@ -37,13 +39,13 @@ export const imageText = {
     aboutGallery19:`Dang, those stupid birthday spankins -Joshua`,
     aboutGallery20:`I remember one time I made brownies for the first time and they were pretty bad but you were nice and pretended
         like they weren't that bad -Joshua`,
-    aboutGallery21:`-Caleb`,
+    aboutGallery21:`Doing some good old Saturday yard work or house work that I complained about -Caleb`,
     aboutGallery22:`One time, when we were about to head to Grandma's house, we were already in the van and you were telling us
         goodbye and you offered to buy me a razor scooter if I stayed home with you. I reaaaally wanted one of those scooters, but
         I also really loved going to Gradma's, so I sat there not knowing what to do for a long minute. I eventually decided to go
         to Grandma's, but I still wonder to this day if you would have actually bought me one if I'd decided to stay home, or if you
         would have just made me go to Gradma's anyways and you were just trying to see which I wanted more -Sarah`,
-    aboutGallery23:`-Caleb`,
+    aboutGallery23:`Times playing basketball in the driveway (especially when I’d start to get frustrated about missing shots) -Caleb`,
     aboutGallery24:`I remember getting to "help" you renovate my bedroom, and I'm sure I was more of an annoyance than anything, but
         I thought it was so cool that I got to help you saw and paint and stuff -Sarah`,
     aboutGallery25:`I can't believe you actually put up with quizzing us on spelling sheets and multiplication cards when we were
