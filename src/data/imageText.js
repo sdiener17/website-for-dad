85a13e5c580e35d43a84d9d45078e747d1ceb167
Sarah Diener <sdiener@ksu.edu>
2022-06-19 02:33:29 -0500

    Messed with image text

--- src/data/imageText.js
+++ src/data/imageText.js
@@ -7,41 +7,42 @@ export const imageText = {
     hexagon6:`You're an awesome chef`,
     aboutGallery1:`Once, you made a joke about a donkey in a hole being an asshole at supper, and we all thought that was the funniest
         thing ever- Sarah`,
-    aboutGallery2:``,
-    aboutGallery3:``,
-    aboutGallery4:``,
+    aboutGallery2:`-Joshua`,
+    aboutGallery3:`-Caleb`,
+    aboutGallery4:`-Joshua`,
     aboutGallery5:`I always love hearing the stories from the cousins about the mischief you got into with them around Christmas
         every year, like putting their lifesavers boxes in a wooden box -Sarah`,
-    aboutGallery6:``,
+    aboutGallery6:`-Caleb`,
     aboutGallery7:`You used to sometimes come in and dump ice in on us while we were taking showers and you thought that was just
         sooooo funny -Sarah`,
-    aboutGallery8:``,
-    aboutGallery9:``,
-    aboutGallery10:``,
-    aboutGallery11:``,
-    aboutGallery12:``,
+    aboutGallery8:`-Caleb`,
+    aboutGallery9:`-Caleb`,
+    aboutGallery10:`-Joshua`,
+    aboutGallery11:`-Caleb`,
+    aboutGallery12:`-Joshua`,
     aboutGallery13:`I remember you used to sometimes grab some marshmallows and start throwing them at us whenever you used them
         for something like hot chocolate or rice crispy treats -Sarah`,
-    aboutGallery14:``,
-    aboutGallery15:``,
+    aboutGallery14:`-Caleb`,
+    aboutGallery15:`-Joshua`,
     aboutGallery16:`You were always there for me when I was struggling with my math homework, and you somehow put up with me even
         when I started yelling at you because I was so frustrated -Sarah`,
-    aboutGallery17:``,
+    aboutGallery17:`-Joshua`,
     aboutGallery18:`I remember once, when we lived in Newton, I was sick and threw up in the middle of the night. You took me
         downstairs and got me some Cheerios and yogurt to eat and we sat in the living room and watched Care Bears for a while.
         I thought that was the coolest thing ever and wanted to be sick all the time so I could do that again -Sarah`,
-    aboutGallery19:``,
-    aboutGallery20:``,
-    aboutGallery21:``,
+    aboutGallery19:`-Joshua`,
+    aboutGallery20:`-Joshua`,
+    aboutGallery21:`-Caleb`,
     aboutGallery22:`One time, when we were about to head to Grandma's house, we were already in the van and you were telling us
         goodbye and you offered to buy me a razor scooter if I stayed home with you. I reaaaally wanted one of those scooters, but
         I also really loved going to Gradma's, so I sat there not knowing what to do for a long minute. I eventually decided to go
         to Grandma's, but I still wonder to this day if you would have actually bought me one if I'd decided to stay home, or if you
         would have just made me go to Gradma's anyways and you were just trying to see which I wanted more -Sarah`,
-    aboutGallery23:``,
+    aboutGallery23:`-Caleb`,
     aboutGallery24:`I remember getting to "help" you renovate my bedroom, and I'm sure I was more of an annoyance than anything, but
         I thought it was so cool that I got to help you saw and paint and stuff -Sarah`,
-    aboutGallery25:``,
+    aboutGallery25:`I can't believe you actually put up with quizzing us on spelling sheets and multiplication cards when we were
+        so annoyed at having to do them. Thanks for doing that for us! -Sarah`,
     homeGallery1:`You're super awesome`,
     homeGallery2:`so we thought we'd give you`,
     homeGallery3:`a website that's`,
